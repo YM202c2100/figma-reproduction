a15afe285700494c855810a7aead5b1ab8525a06
YM202c2100 <y.m202c2100@gmail.com>
2024-05-08 22:47:02 +0900

    [add] Headerコンポーネントを表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+// import Header from "app/components/header.tsx"
+import Header from "./components/header"
 
 export default function Page() {
-  return (
-    <h1>Initial State</h1>
-  )
+  return (<>
+    <Header/>
+    </>)
 }
