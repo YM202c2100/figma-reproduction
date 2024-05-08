e07ce26ab667d67172ec83fcdec4a95e996f07b0
YM202c2100 <y.m202c2100@gmail.com>
2024-05-08 22:45:17 +0900

    [add] headerコンポーネントの新規追加

---
+++ app/components/header.tsx
@@ -0,0 +1,19 @@
+
+function Header(){
+  return(
+    <header>
+      <div>Site name</div>
+      <nav>
+        <ul>
+          <li>Page1</li>
+          <li>Page2</li>
+          <li>Page3</li>
+        </ul>
+        <button>Button</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
+
