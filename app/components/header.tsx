6816d0e450564c2e10a1153fd9e884065dca425d
YM202c2100 <y.m202c2100@gmail.com>
2024-05-11 16:37:45 +0900

    [update] ヘッダー部分の文字サイズをレスポンシブ対応
    lg,xl,2xlで変化

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,14 +2,14 @@
 function Header(){
   return(
     <header className="flex flex-col items-center h-28 justify-center md:flex-row md:justify-between">
-      <div className="md:ml-12">Site name</div>
+      <div className="md:ml-12 lg:text-2xl xl:text-3xl">Site name</div>
       <nav className="flex">
-        <ul className="flex items-center">
-          <li className="">Page1</li>
-          <li className="ml-8">Page2</li>
-          <li className="ml-8">Page3</li>
+        <ul className="flex items-center lg:text-2xl 2xl:text-3xl">
+          <li className="mr-4">Page1</li>
+          <li className="mx-4">Page2</li>
+          <li className="mx-4">Page3</li>
         </ul>
-        <button className="text-xs ml-10 md:mr-10 bg-black text-white px-4 py-3 rounded-md">Button</button>
+        <button className="text-xs ml-3 md:mr-10 bg-black text-white px-4 py-3 rounded-md lg:text-sm xl:text-xl">Button</button>
       </nav>
     </header>
   )
