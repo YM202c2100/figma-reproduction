ac63cd7f8829de9cfebb6f24cabed4a150442bcd
YM202c2100 <y.m202c2100@gmail.com>
2024-05-11 18:30:36 +0900

    [add] Featuresの中身をFeatureコンポーネントとして分離

--- app/components/aboutFeatures.tsx
+++ app/components/aboutFeatures.tsx
@@ -1,30 +1,16 @@
 import Image from "next/image"
+import Feature from "./feature"
 
 export default function AboutFeatures(){
   return(
     <div>
       <h1 className="text-4xl mb-5">Heading</h1>
 
-      <div className="flex flex-wrap">
-        <div className="w-full md:w-1/2 ">
-          {/* <Image/> */}
-          <h2 className="text-2xl">Subheading</h2>
-          <p className="text-stone-400 text-xl">
-            Body text for whatever you'd like to say. Add main
-            takeaway points, quotes, anecdotes, or even a very
-            very short story. 
-          </p>
-        </div>
-
-        <div className="w-full md:w-1/2 ">
-          {/* <Image/> */}
-          <h2 className="text-2xl">Subheading</h2>
-          <p className="text-stone-400 text-xl">
-            Body text for whatever you'd like to say. Add main
-            takeaway points, quotes, anecdotes, or even a very
-            very short story. 
-          </p>
-        </div>
+      <div className="flex flex-wrap justify-between">
+        <Feature/>
+        <Feature/>
+        <Feature/>
+        <Feature/>
       </div>
     </div>
   )
---
+++ app/components/feature.tsx
@@ -0,0 +1,14 @@
+
+export default function Feature(){
+  return(
+    <div className="w-3/4 py-7 lg:w-2/5">
+      {/* <Image/> */}
+      <h2 className="text-2xl">Subheading</h2>
+      <p className="text-stone-400 text-xl">
+        Body text for whatever you'd like to say. Add main
+        takeaway points, quotes, anecdotes, or even a very
+        very short story. 
+      </p>
+    </div>
+  )
+}
