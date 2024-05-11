bf2f387dbb187b019e81f2cb58deacb767133b25
YM202c2100 <y.m202c2100@gmail.com>
2024-05-11 18:00:45 +0900

    [update] flexwrapを用いてレスポンシブ化
    md未満では立て並びに、md以上では一列につき二つずつ並ぶように

--- app/components/aboutFeatures.tsx
+++ app/components/aboutFeatures.tsx
@@ -3,23 +3,27 @@ import Image from "next/image"
 export default function AboutFeatures(){
   return(
     <div>
-      <div>
-        <div>
-        {/* <Image/> */}
-        <h4>Subheading</h4>
-        <p className="text-stone-400">
-          Body text for whatever you'd like to say. Add main
-          takeaway points, quotes, anecdotes, or even a very
-          very short story. 
-        </p>
+      <h1 className="text-4xl mb-5">Heading</h1>
+
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-1/2 ">
+          {/* <Image/> */}
+          <h2 className="text-2xl">Subheading</h2>
+          <p className="text-stone-400 text-xl">
+            Body text for whatever you'd like to say. Add main
+            takeaway points, quotes, anecdotes, or even a very
+            very short story. 
+          </p>
         </div>
-        <div>
-        <h4>Subheading</h4>
-        <p className="text-stone-400">
-          Body text for whatever you'd like to say. Add main
-          takeaway points, quotes, anecdotes, or even a very
-          very short story. 
-        </p>
+
+        <div className="w-full md:w-1/2 ">
+          {/* <Image/> */}
+          <h2 className="text-2xl">Subheading</h2>
+          <p className="text-stone-400 text-xl">
+            Body text for whatever you'd like to say. Add main
+            takeaway points, quotes, anecdotes, or even a very
+            very short story. 
+          </p>
         </div>
       </div>
     </div>
