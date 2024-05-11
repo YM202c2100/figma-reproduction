eba7b1684cea14659e6b1ef7b88fdc746a28e7fd
YM202c2100 <y.m202c2100@gmail.com>
2024-05-11 14:42:23 +0900

    [update] 写真の角を丸める

--- app/components/cardGenerator.tsx
+++ app/components/cardGenerator.tsx
@@ -18,7 +18,7 @@ function CardGenerator(){
         {Cards.map((card, i) => {
             return(
                 <div key={card.src.src} className={`mx-5 mb-10 md:mb-20  md:flex ${i%2 == 0 ? "":"md:flex-row-reverse"}`}>
-                    <Image src={card.src} alt="Picture of OO" className="md:w-1/2 "/>
+                    <Image src={card.src} alt="Picture of OO" className="md:w-1/2 rounded-xl"/>
                     <div className={`flex-grow py-10 flex flex-col justify-center ${i%2 == 0 ?"md:pl-16": "md:pr-16"}`}>
                         <div className="text-4xl">{card.title}</div>
                         <div className="text-stone-400 text-xl my-4">{card.description}</div>
