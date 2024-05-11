1cea94715585fdda754a3d2cb246ee33f738389d
YM202c2100 <y.m202c2100@gmail.com>
2024-05-11 15:38:26 +0900

    [update] Header内のレスポンシブ対応
    メディアクエリによる指定(lg, xl, 2xl)

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Page() {
       <Header/>
       {/* メインタイトル */}
       <div className="flex flex-col items-center">
-        <div className="text-5xl mt-20">Landing page title</div>
-        <div className="m-4 text-stone-400">
+        <div className="text-6xl mt-20 lg:text-7xl xl:text-8xl 2xl:text-9xl">Landing page title</div>
+        <div className="text-lg m-4 text-stone-400 lg:text-2xl xl:text-3xl xl:my-8 2xl:text-5xl">
           And a subheading describing you site, too
         </div>
-        <button className="text-xs mx-10 bg-black text-white px-4 py-3 rounded-md">Button</button>
+        <button className="text-xs bg-black text-white px-4 py-3 rounded-md lg:text-xl xl:text-3xl xl:px-8 xl:py-6">Button</button>
       </div>
       {/* Content description */}
       <div className="container mx-auto my-28">
