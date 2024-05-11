02c46f3e4f095fc269b22c13a8f0c2501b0583e2
YM202c2100 <y.m202c2100@gmail.com>
2024-05-11 20:24:18 +0900

    [update] アイコンのスタイリング

--- app/components/feature.tsx
+++ app/components/feature.tsx
@@ -6,7 +6,7 @@ type srcProps = {
 const Feature:React.FC<srcProps> = ({src})=>{
   return(
     <div className="w-3/4 py-7 lg:w-2/5">
-      <Image src={src} alt=""/>
+      <Image src={src} alt={src.src} width={50} height={50} className="relative right-1"/>
       <h2 className="text-2xl">Subheading</h2>
       <p className="text-stone-400 text-xl">
         Body text for whatever you'd like to say. Add main
